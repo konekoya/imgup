2cb512fd95e3c6af567fa9bee1feaa0c32a8260b
Joshua <tank158r5155@yahoo.com.tw>
2023-02-15 16:53:44 +0800

    Refactor

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,113 @@
+import axios, { AxiosRequestConfig } from 'axios';
+import chalk from 'chalk';
 import { Command, program } from 'commander';
-import { getPackageMeta } from './utils.js';
+import formData from 'form-data';
+import inquirer, { Answers } from 'inquirer';
+import fs from 'node:fs';
+import path from 'node:path';
+import ora from 'ora';
+import ConfigStore from './config-store.js';
+import { API_URL, CONFIG_KEY } from './const.js';
+import { UploadParams } from './types.js';
+import { getFilename, getPackageMeta, isImage } from './utils.js';
+
+const configStore = ConfigStore.getInstance();
 
 export function createProgram(): Command {
   const { version: appVersion } = getPackageMeta();
   program.version(appVersion, '-v, --version', 'output the current version');
   return program;
+}
+
+// Load client ID from the configstore
+export function getClientId(): string {
+  const clientId: string | undefined = configStore.get(CONFIG_KEY);
+  if (clientId === undefined) {
+    console.log(
+      chalk.red(
+        'No client ID found in your config. Run `imgup config` to add one.',
+      ),
+    );
+    process.exit(1);
+  }
+
+  return clientId;
+}
+
+// Ask users for the client ID, and persist in the configstore locally
+// the config can be found in `~/.config/configstore/imgup.json`
+export function configureClientId() {
+  inquirer
+    .prompt([
+      {
+        name: 'clientId',
+        message: `Enter your client ID:`,
+        validate: async (input) => {
+          if (input.trim() === '') {
+            return 'Please enter a correct client ID';
+          }
+          return true;
+        },
+      },
+    ])
+    .then((answers: Answers) => {
+      configStore.set(CONFIG_KEY, answers.clientId.trim());
+    })
+    .catch((error: Error) => {
+      console.log(error);
+      process.exit(1);
+    });
+}
+
+// Upload image to the services via the API endpoint, the returned data contains
+// the like to the image and the filename from the image file
+export async function uploadImage({
+  imagePath,
+  clientId,
+}: UploadParams): Promise<string | void> {
+  const data = new formData();
+  const fileName = getFilename(imagePath);
+  data.append('image', fs.createReadStream(imagePath));
+  data.append('name', fileName);
+  data.append('title', fileName);
+
+  const config = {
+    method: 'post',
+    url: API_URL,
+    headers: {
+      Authorization: `Client-ID ${clientId}`,
+      ...data.getHeaders(),
+    },
+    data: data,
+  } as AxiosRequestConfig;
+
+  const spinner = ora('Uploading image to imgur.com').start();
+  try {
+    const res = await axios(config);
+    spinner.succeed('Success');
+    return res.data.data.link;
+  } catch (error) {
+    spinner.fail(
+      chalk.red("Oops, something went wrong! We're able to upload your image."),
+    );
+    console.log(chalk.yellow('Did you use a valid Client ID?'));
+    console.dir(error, { depth: Infinity });
+  }
+}
+
+// See if the image path is valid
+export async function resolveImagePath(imagePath: string): Promise<string> {
+  const resolvedPath = path.resolve(process.cwd(), imagePath);
+
+  if (!fs.existsSync(resolvedPath)) {
+    console.log(`${resolvedPath} doesn't exist!`);
+    process.exit(1);
+  }
+
+  if (await isImage(resolvedPath)) {
+    return resolvedPath;
+  } else {
+    console.log(`${resolvedPath} is not an image!`);
+    process.exit(1);
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,16 @@
 #!/usr/bin/env node
 
-import axios, { AxiosRequestConfig } from 'axios';
 import chalk from 'chalk';
-import formData from 'form-data';
-import inquirer, { Answers } from 'inquirer';
 import capitalize from 'lodash/fp/capitalize.js';
-import fs from 'node:fs';
-import ora from 'ora';
-import path from 'path';
-import { createProgram } from './cli.js';
-import ConfigStore from './config-store.js';
-import { API_URL, CONFIG_KEY } from './const.js';
-import { UploadParams } from './types.js';
-import { getFilename, isImage } from './utils.js';
+import {
+  configureClientId,
+  createProgram,
+  getClientId,
+  resolveImagePath,
+  uploadImage,
+} from './cli.js';
+import { getFilename } from './utils.js';
 
-const configStore = ConfigStore.getInstance();
-
-// CLI
 const program = createProgram();
 
 program
@@ -47,96 +41,3 @@ program
   .action(() => configureClientId());
 
 program.parse(process.argv);
-
-// Load client ID from the configstore
-function getClientId(): string {
-  const clientId: string | undefined = configStore.get(CONFIG_KEY);
-  if (clientId === undefined) {
-    console.log(
-      chalk.red(
-        'No client ID found in your config. Run `imgup config` to add one.',
-      ),
-    );
-    process.exit(1);
-  }
-
-  return clientId;
-}
-
-// Upload image to the services via the API endpoint, the returned data contains
-// the like to the image and the filename from the image file
-async function uploadImage({
-  imagePath,
-  clientId,
-}: UploadParams): Promise<string | void> {
-  const data = new formData();
-  const fileName = getFilename(imagePath);
-  data.append('image', fs.createReadStream(imagePath));
-  data.append('name', fileName);
-  data.append('title', fileName);
-
-  const config = {
-    method: 'post',
-    url: API_URL,
-    headers: {
-      Authorization: `Client-ID ${clientId}`,
-      ...data.getHeaders(),
-    },
-    data: data,
-  } as AxiosRequestConfig;
-
-  const spinner = ora('Uploading image to imgur.com').start();
-  try {
-    const res = await axios(config);
-    spinner.succeed('Success');
-    return res.data.data.link;
-  } catch (error) {
-    spinner.fail(
-      chalk.red("Oops, something went wrong! We're able to upload your image."),
-    );
-    console.log(chalk.yellow('Did you use a valid Client ID?'));
-    console.dir(error, { depth: Infinity });
-  }
-}
-
-// See if the image path is valid
-async function resolveImagePath(imagePath: string): Promise<string> {
-  const resolvedPath = path.resolve(process.cwd(), imagePath);
-
-  if (!fs.existsSync(resolvedPath)) {
-    console.log(`${resolvedPath} doesn't exist!`);
-    process.exit(1);
-  }
-
-  if (await isImage(resolvedPath)) {
-    return resolvedPath;
-  } else {
-    console.log(`${resolvedPath} is not an image!`);
-    process.exit(1);
-  }
-}
-
-// Ask users for the client ID, and persist in the configstore locally
-// the config can be found in `~/.config/configstore/imgup.json`
-function configureClientId() {
-  inquirer
-    .prompt([
-      {
-        name: 'clientId',
-        message: `Enter your client ID:`,
-        validate: async (input) => {
-          if (input.trim() === '') {
-            return 'Please enter a correct client ID';
-          }
-          return true;
-        },
-      },
-    ])
-    .then((answers: Answers) => {
-      configStore.set(CONFIG_KEY, answers.clientId.trim());
-    })
-    .catch((error: Error) => {
-      console.log(error);
-      process.exit(1);
-    });
-}
